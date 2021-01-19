3e08e287a2e8de2c360638eaa1c2a1924afeea97
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-01-19 14:22:04 -0800

    finish products obj array

--- products/garments.js
+++ products/garments.js
@@ -8,4 +8,49 @@ export const garments = [
         price : 15
     },
 
+    {
+        id : 2,
+        name : 'Hat',
+        image : '../assets/hat.png',
+        description : 'A warm and cozy crochet beanie.',
+        category : 'clothing',
+        price : 20
+    },
+
+    {
+        id : 3,
+        name : 'Headband',
+        image : '../assets/headband.png',
+        description : 'A stylish and warm headband.',
+        category : 'clothing',
+        price : 10
+    },
+
+    {
+        id : 4,
+        name : 'Mike',
+        image : '../assets/mike.png',
+        description : 'Michael Wazowski also known as Mike, is a main character in Monsters, Inc.',
+        category : 'other',
+        price : 2319
+    },
+
+    {
+        id : 5,
+        name : 'Scarf',
+        image : '../assets/scarf.png',
+        description : 'A long and luxurious crochet scarf!',
+        category : 'clothing',
+        price : 30
+    },
+
+    {
+        id : 6,
+        name : 'Sweater',
+        image : '../assets/sweater.png',
+        description : 'A charming and warm oversized sweater.',
+        category : 'clothing',
+        price : 15
+    },
+
 ];

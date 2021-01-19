4bbbd002c8ec2d15fac5dbca7a5ceda49143c3ae
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-01-19 14:07:24 -0800

    create hard coded product template with basic styling

---
+++ products/garments.js
@@ -0,0 +1,11 @@
+export const garments = [
+    {
+        id : 1,
+        name : 'Mittens',
+        image : '../assets/mittens.png',
+        description : 'A warm and cozy pair of mittens!',
+        category : 'clothing',
+        price : 15
+    },
+
+];

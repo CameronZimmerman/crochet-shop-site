673f2303433d92a72b4c27a6f9384f284c44174f
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-01-20 14:11:29 -0800

    create test for find by Id function

--- test/example.test.js
+++ test/example.test.js
@@ -1,8 +1,31 @@
 // IMPORT MODULES under test here:
 import { garments } from '../products/garments.js';
 import { renderGarments } from '../products/garments-render.js';
+import { findByID } from '../utils.js';
  
 const test = QUnit.test;
+
+//for find by ID test
+const garments = [
+    {
+        id : 1,
+        name : 'Mittens',
+        image : '../assets/mittens.png',
+        description : 'A warm and cozy pair of mittens!',
+        category : 'clothing',
+        price : 15 
+    },
+
+    {
+        id : 2,
+        name : 'Blanket',
+        image : '../assets/mittens.png',
+        description : 'A warm and cozy pair of mittens!',
+        category : 'clothing',
+        price : 15 
+    }
+
+];
 
 //render function test
 test('given obj, return correctly filled HTML string ', (expect) => {
@@ -18,3 +41,19 @@ test('given obj, return correctly filled HTML string ', (expect) => {
     // Make assertions about what is expected versus the actual result
     expect.equal(actual.outerHTML, expected);
 });
+
+//findByID function test
+test('given an ID and an array, return corresponding obj from arr with same ID', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = garments[0];
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = findByID(1, garments);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.deepEqual(actual, expected);
+});
+

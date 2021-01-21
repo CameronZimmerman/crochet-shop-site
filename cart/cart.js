c746899da0361a1b586a502d3589219a2aba4307
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-01-20 16:32:26 -0800

    add style to table

--- cart/cart.js
+++ cart/cart.js
@@ -3,7 +3,7 @@
 import { cartData } from './cart-data.js';
 import { renderCartRow } from './render-cart-row.js';
 import { garments } from '../products/garments.js';
-import { findByID } from '../utils.js';
+import { findByID, calcOrderTotal } from '../utils.js';
 
 //grab dom elements
 
@@ -12,6 +12,18 @@ const table = document.querySelector('table');
 for (const cartItem of cartData) {
     const garment = findByID(cartItem.id, garments);
 
-    const newTR = renderCartRow(cartItem, garment);
-    table.append(newTR);
+    const newTr = renderCartRow(cartItem, garment);
+    table.append(newTr);
 }
+
+const totalTr = document.createElement('tr');
+
+const totalTd1 = document.createElement('td');
+const totalTd2 = document.createElement('td');
+const totalTd3 = document.createElement('td');
+totalTr.append(totalTd1, totalTd2, totalTd3);
+
+totalTd1.textContent = 'Total:';
+totalTd3.textContent = `$${calcOrderTotal(cartData, garments)}`;
+
+table.append(totalTr);

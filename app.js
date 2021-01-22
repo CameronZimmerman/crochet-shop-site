58e11397ffe82007258e3ec594e5a52887b86d1d
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-01-21 18:00:25 -0800

    fix home page counter

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-import { getCart } from './cart/cart-api.js';
+import { updateCartCount } from './utils.js';
 
-const CART = 'cart';
-const cartCountSpan = document.getElementById('cart-amount');
-
-const cartData = getCart(CART);
-
-cartCountSpan.textContent = cartData.length;
+updateCartCount();

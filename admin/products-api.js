379ea914c3c54f6dd24b4af1d1166408b441f76b
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-01-22 11:09:07 -0800

    add getAndSetProducts function

---
+++ admin/products-api.js
@@ -0,0 +1,12 @@
+
+import { garments } from '../products/garments.js';
+const PRODUCTS = 'products';
+
+export function getAndSeedProducts() {
+    let products = JSON.parse(localStorage.getItem(PRODUCTS) || '[]');
+    if (products.length === 0) products = garments;
+    localStorage.setItem(PRODUCTS, JSON.stringify(products));
+
+    return products;
+
+}
